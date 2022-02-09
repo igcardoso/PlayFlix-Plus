8a180443df33f54b22e3e530b70592f5fb0e6c2d
igor <85964276+igcardoso@users.noreply.github.com>
2022-02-09 18:16:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function channels() {
+  window.alert('Conteúdo de transmissão Ao vivo dos canais do YouTube oficiais de TV online.');
+}
+
+function trailer(el) {
+  var display = document.getElementById(el).style.display;
+  if (display == "none")
+    document.getElementById(el).style.display = 'block';
+  else
+    document.getElementById(el).style.display = 'none';
+}
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
