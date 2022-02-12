f3b2ccea46b865d12290a03fa7894324aa5c3210
igor <85964276+igcardoso@users.noreply.github.com>
2022-02-11 21:23:01 -0300

    update script.js

--- script.js
+++ script.js
@@ -10,14 +10,6 @@ function trailer(el) {
     document.getElementById(el).style.display = 'none';
 }
 
-function titleTrailerCartais(el) {
-  var display = document.getElementById(el).style.display;
-  if (display == "none")
-    document.getElementById(el).style.display = 'block';
-  else
-    document.getElementById(el).style.display = 'none';
-}
-
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 3,
   spaceBetween: 30,
